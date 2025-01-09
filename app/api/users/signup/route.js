d3089c4e86568818773d7f04fa31ce89148a2347
asiradnan <asiradnan23@gmail.com>
2025-01-09 14:44:27 +0600

    Fixed

--- app/api/users/signup/route.js
+++ app/api/users/signup/route.js
@@ -8,7 +8,8 @@ await connectDB()
 export async function POST(request) {
     try {
         const body = await request.json()
-        const {name, university_email, id, password, role} = body
+        console.log(body)
+        const {name, university_email, id, password} = body
         var user = await User.findOne({university_email})
         if (user) {
             return NextResponse.json({error:"The email is already registred"},{status:400})
@@ -20,6 +21,9 @@ export async function POST(request) {
         if (university_email.split("@")[1] != "g.bracu.ac.bd" && university_email.split("@")[1] != "bracu.ac.bd") {
             return NextResponse.json({error:"Only BRACU student/faculty can register"},{status:400})
         }
+        var role;
+        if (university_email.split("@")[1] != "g.bracu.ac.bd") role = "student"
+        else role = "faculty"
         const salt = await bcryptjs.genSalt(10)
         const hashedPassword = await bcryptjs.hash(password,salt)
         const newUser = new User({name, university_email,id, password: hashedPassword, role: role})
